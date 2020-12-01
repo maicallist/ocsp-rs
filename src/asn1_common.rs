8c5c3f7caa12f4e47ed9c91d20d753040c4d2a07
gaochen9 <gaochen9@cnpc.com.cn>
2020-12-01 12:45:35 +0800

    impl IntoSequence for DerObject

--- src/asn1_common.rs
+++ src/asn1_common.rs
@@ -1,3 +1,8 @@
+use super::err::OcspError;
+use asn1_der::{
+    typed::{DerDecodable, Sequence},
+    DerObject,
+};
 /// see [ocsp_rs::asn1_req::OcspRequestAsn1::extract_certid()]
 pub(crate) const CERTID_TAG: [u8; 5] = [6u8, 5u8, 4u8, 4u8, 2u8];
 
@@ -12,3 +17,17 @@ pub(crate) fn count_match_tags(target: &Vec<u8>, tbm: &Vec<u8>) -> usize {
     let partial = &target[0..tbm.len()];
     tbm.iter().zip(partial).filter(|(t, p)| t == p).count()
 }
+
+pub trait IntoSequence {
+    type Error;
+    fn try_into(d: DerObject) -> Result<Sequence, Self::Error>;
+}
+
+impl IntoSequence for DerObject<'_> {
+    type Error = OcspError;
+    fn try_into(der: DerObject) -> Result<Sequence, Self::Error> {
+        Sequence::decode(der.raw()).map_err(OcspError::Asn1DecodingError)
+    }
+}
+
+pub trait DecodeAsn1 {}
