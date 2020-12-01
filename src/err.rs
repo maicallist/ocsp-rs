d335053f5fac0c09ff08a82b5dfa1dc6956b9c79
gaochen9 <gaochen9@cnpc.com.cn>
2020-12-01 12:43:37 +0800

    add docs for each error variant

--- src/err.rs
+++ src/err.rs
@@ -2,10 +2,13 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum OcspError {
+    /// raw data does not start with a sequence
     #[error("Expecting data start with Sequence")]
     Asn1UnexpectedType,
+    /// cannot convert raw data into asn1_der::typed::*;
     #[error("Unable to decode ASN1 data")]
     Asn1DecodingError(#[from] asn1_der::Asn1DerError),
+    /// unexpected result from sequence matching fn
     #[error("Unable to extract data from asn1 due to traversal issue")]
     Asn1ExtractionUnknownError,
 }
