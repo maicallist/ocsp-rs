921fc96f2169ebd2475f8bcf8e46e9cea58e2db2
gaochen9 <gaochen9@cnpc.com.cn>
2020-12-09 16:03:29 +0800

    add docs

--- src/common.rs
+++ src/common.rs
@@ -1,3 +1,5 @@
+//! common traits and const
+
 use asn1_der::{
     typed::{DerDecodable, Sequence},
     DerObject,
@@ -5,9 +7,11 @@ use asn1_der::{
 
 use crate::err::OcspError;
 
+/// CertID asn1 sequence
+/// see [ocsp_rs::asn1_common::OcspAsn1Der::extract_id]
 pub(crate) const CERTID_TAG: [u8; 5] = [6u8, 5u8, 4u8, 4u8, 2u8];
 
-/// allow convert to asn1_der::typed::Sequence
+/// allowing data to be converted to asn1_der::typed::Sequence
 pub trait TryIntoSequence {
     /// converting asn1_der::err
     type Error;
