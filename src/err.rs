0af9d963ca699869d1e218684a0b1f4b0dda906c
gaochen9 <gaochen9@cnpc.com.cn>
2020-12-24 16:47:12 +0800

    add more explaination on error

--- src/err.rs
+++ src/err.rs
@@ -7,7 +7,7 @@ pub enum OcspError {
     #[error("Expecting data start with Sequence")]
     Asn1UnexpectedType,
     /// cannot convert raw data into asn1_der::typed::*;
-    #[error("Unable to decode ASN1 data")]
+    #[error("Unable to decode ASN1 data, originated from asn1_der crate")]
     Asn1DecodingError(#[from] asn1_der::Asn1DerError),
     /// unexpected result from sequence matching fn
     #[error("Unable to extract data from asn1 due to traversal issue")]
