d593adbe02be1c49aeabf9b221d0c12fb7c953a7
gaochen9 <gaochen9@cnpc.com.cn>
2020-12-29 09:49:36 +0800

    remove unused error, add location macro

--- src/err.rs
+++ src/err.rs
@@ -3,24 +3,21 @@ use thiserror::Error;
 /// crate error enum
 #[derive(Error, Debug)]
 pub enum OcspError {
-    /// raw data does not start with a sequence
-    #[error("Expecting data start with Sequence")]
-    Asn1UnexpectedType,
     /// cannot convert raw data into asn1_der::typed::*;
-    #[error("Unable to decode ASN1 data, originated from asn1_der crate")]
+    #[error(transparent)]
     Asn1DecodingError(#[from] asn1_der::Asn1DerError),
-    /// unexpected result from sequence matching fn
-    #[error("Unable to extract data from asn1 due to traversal issue")]
-    Asn1ExtractionUnknownError,
+
     /// extractor cannot find matching sequence  
     /// eg. OID sequence is not 0x06, 0x05
     #[error("Unable to extract desired sequence of {0}")]
     Asn1MismatchError(String),
+
     /// ocsp request contains unexpected data
     /// case 1: no sequence in request
     /// case 2: ocsp request is not {0x30} or {0x30, 0xA0}
     #[error("Ocsp request contains unexpected data")]
     Asn1MalformedTBSRequest,
+
     /// unable to parse vec\<u8\> to &str   
     /// eg. requestorName
     #[error("Unable to deserialize string from ocsp req/resp")]
@@ -37,4 +34,12 @@ pub enum OcspError {
     /// Cannot find OID in predefined list
     #[error("Unable to locate OID info")]
     Asn1OidUnknown,
+}
+
+/// display error location
+#[macro_export]
+macro_rules! error_origin {
+    () => {
+        concat!("at ", file!(), " line ", line!(), " column ", column!())
+    };
 }
