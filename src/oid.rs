35e57c6fac4fe414bbe2adf9bb4fd9eaaa0b2543
gaochen9 <gaochen9@cnpc.com.cn>
2020-12-31 13:39:22 +0800

    update doc on oid mod

--- src/oid.rs
+++ src/oid.rs
@@ -16,20 +16,24 @@ pub(crate) const OCSP_EXT_CRLREF_NUM: &str = "1.3.6.1.5.5.7.48.1.3";
 pub(crate) const OCSP_EXT_CRLREF_NAME: &str = "id-pkix-ocsp 3";
 
 /// converting oid to human-readable num
-/// see https://docs.microsoft.com/en-us/windows/win32/seccertenroll/about-object-identifier?redirectedfrom=MSDN
+/// see [doc](https://docs.microsoft.com/en-us/windows/win32/seccertenroll/about-object-identifier?redirectedfrom=MSDN)
 pub async fn b2v_oid(oid: Vec<u8>) -> u8 {
     unimplemented!()
 }
 
-/// defined OID
+/// OID info
 #[derive(Debug)]
 pub struct ConstOid {
+    /// internal id for OID
     pub(crate) id: u8,
+    /// OID in number format, eg. 1.3.6.1.3.5
     num: &'static str,
+    /// OID in text format, eg. pkix-ocsp 1
     name: &'static str,
 }
 
 lazy_static! {
+    /// predefined OID list
     pub(crate) static ref OID_LIST: HashMap<Vec<u8>, ConstOid> = vec![
         (
             OCSP_EXT_NONCE_HEX.to_vec(),
