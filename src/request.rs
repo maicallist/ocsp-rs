65ff80a3182f696c6cd134b0e6c9e94e49f9affa
gaochen9 <gaochen9@cnpc.com.cn>
2021-01-28 15:25:00 +0800

    add test for multiple cert in request

--- src/request.rs
+++ src/request.rs
@@ -250,6 +250,15 @@ impl OcspRequest {
             optional_signature: sig,
         })
     }
+
+    /// extract all cert serial numbers from request
+    pub async fn extract_cert_sn(&self) -> Vec<Vec<u8>> {
+        let mut sn = vec![];
+        let list = &self.tbs_request.request_list;
+        list.into_iter()
+            .for_each(|r| sn.push(r.certid.serial_num.clone()));
+        sn
+    }
 }
 
 #[cfg(test)]
@@ -274,6 +283,26 @@ mod test {
         let reg = Registry::default().with(env_layer).with(fmt_layer);
         tracing_log::LogTracer::init().unwrap();
         tracing::subscriber::set_global_default(reg).unwrap();
+    }
+
+    // test two certs in request
+    #[tokio::test]
+    async fn ocsp_two_cert_req() {
+        let ocsp_req_hex = "3081B53081B230818A30433041300906\
+    052B0E03021A05000414694D18A9BE42\
+    F7802614D4844F23601478B788200414\
+    397BE002A2F571FD80DCEB52A17A7F8B\
+    632BE755020841300983331F9D4F3043\
+    3041300906052B0E03021A0500041469\
+    4D18A9BE42F7802614D4844F23601478\
+    B788200414397BE002A2F571FD80DCEB\
+    52A17A7F8B632BE75502086378E51D44\
+    8FF46DA2233021301F06092B06010505\
+    07300102041204105E7A74E51C861A3F\
+    79454658BB090244";
+        let req_v8 = hex::decode(ocsp_req_hex).unwrap();
+        let req = OcspRequest::parse(&req_v8[..]).await;
+        assert!(req.is_ok());
     }
 
     #[tokio::test]
